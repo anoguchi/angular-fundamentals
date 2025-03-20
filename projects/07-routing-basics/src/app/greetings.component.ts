a1ddd8c0f8319fafa462647d951b44599121dba9
Alberto Noguchi <alberto2611@gmail.com>
2025-03-20 18:38:38 -0300

    add lesson 07 and 08

--- projects/07-routing-basics/src/app/greetings.component.ts
+++ projects/07-routing-basics/src/app/greetings.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-greetings',
+  imports: [RouterModule],
   template: `<p>Hello dear learner 👋🏾</p>`,
   standalone: true,
 })
